946851228024e11c643f5c85d9fb25997ff05f63
An Le <asble@ucdavis.edu>
2018-05-09 10:06:43 -0700

    added mailer functionality and sitemap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ config.webpacker.check_yarn_integrity = false
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "gmail.com",
+    :user_name => ENV["EMAIL_USERNAME"],
+    :password => ENV["EMAIL_USERNAME"],
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
 end
